088f344ab3398c93730329896687703c13d03e01
kwon <kwondw30@gmail.com>
2024-09-21 18:03:03 +0900

    commit

---
+++ src/main/java/com/example/noticeboardprj/NoticeBoardPrjApplication.java
@@ -0,0 +1,13 @@
+package com.example.noticeboardprj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoticeBoardPrjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoticeBoardPrjApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/noticeboardprj/NoticeBoardPrjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.noticeboardprj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoticeBoardPrjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
