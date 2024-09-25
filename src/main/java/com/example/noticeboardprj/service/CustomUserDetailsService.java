acab8e5b236b4e1129e66c50d98249168d897e67
DESKTOP-8MML8S9\kwond <kwondw30@gmail.com>
2024-09-25 17:17:12 +0900

    login process

---
+++ src/main/java/com/example/noticeboardprj/service/CustomUserDetailsService.java
@@ -0,0 +1,31 @@
+package com.example.noticeboardprj.service;
+
+import com.example.noticeboardprj.dto.CustomUserDetails;
+import com.example.noticeboardprj.entity.UserEntity;
+import com.example.noticeboardprj.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        UserEntity userData = userRepository.findByUsername(username);
+
+        if(userData != null) {
+            return new CustomUserDetails(userData);
+        }
+
+        return null;
+    }
+}
