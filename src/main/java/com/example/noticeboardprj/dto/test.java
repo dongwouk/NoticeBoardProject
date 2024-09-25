8373c7615917e8781c3b18a9cfd0debc81f10292
SeokSeungMin <seoksmin@gmail.com>
2024-09-25 09:54:52 +0900

    test code

--- src/main/java/com/example/noticeboardprj/dto/test.java
+++ src/main/java/com/example/noticeboardprj/dto/test.java
@@ -1,8 +1,15 @@
 package com.example.noticeboardprj.dto;
 
+import org.springframework.security.core.Authentication;
+
 public class test {
 
+    public static void main(String[] args) {
+        System.out.println("테스트 코드 주석");
+    }
+
     public void asdf(Authentication auth){
+        //코드 리뷰후 - 승민
 
     }
 }
