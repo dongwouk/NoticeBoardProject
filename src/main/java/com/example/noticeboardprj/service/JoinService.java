1a91164892349322dc4dff5bbc94a03402ca85bb
kwon <kwondw30@gmail.com>
2024-10-09 00:54:19 +0900

    email, 생성/수정/삭제시간 설정

--- src/main/java/com/example/noticeboardprj/service/JoinService.java
+++ src/main/java/com/example/noticeboardprj/service/JoinService.java
@@ -6,6 +6,8 @@ import com.example.noticeboardprj.repository.UserRepository;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+
 @Service
 public class JoinService {
 
@@ -21,10 +23,15 @@ public class JoinService {
 
         String username = joinDTO.getUsername();
         String password = joinDTO.getPassword();
+        String email = joinDTO.getEmail();
+        LocalDateTime created_date = LocalDateTime.now();
+        LocalDateTime updated_date = LocalDateTime.now();
+        LocalDateTime removed_date = LocalDateTime.now();
 
         Boolean isExits = userRepository.existsByUsername(username);
 
         if(isExits) {
+            System.out.println("중복된 아이디!!");
             return;
         }
 
@@ -33,7 +40,13 @@ public class JoinService {
         data.setUsername(username);
         data.setPassword(bCryptPasswordEncoder.encode(password));
         data.setRole("ROLE_ADMIN");
+        data.setEmail(email);
+        data.setCreated_date(created_date);
+        data.setModified_date(updated_date);
+        data.setRemoved_date(removed_date);
 
+
+        System.out.println("===========");
         userRepository.save(data);
     }
 }
