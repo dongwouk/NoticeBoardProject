52efba90163e077e58ccac5380f6ef390e180b17
kwon <kwondw30@gmail.com>
2024-10-09 00:59:46 +0900

    refresh token 유무, 삭제 메서드

---
+++ src/main/java/com/example/noticeboardprj/repository/RefreshRepository.java
@@ -0,0 +1,16 @@
+package com.example.noticeboardprj.repository;
+
+import com.example.noticeboardprj.entity.RefreshEntity;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RefreshRepository extends JpaRepository<RefreshEntity, Long> {
+
+    // refresh token 존재여부 확인
+    Boolean existsByRefresh(String refresh);
+
+    // refresh token delete 메서드
+    // DB에 적용하므로 transactional 어노테이션 설정
+    @Transactional
+    void deleteByRefresh(String refresh);
+}
