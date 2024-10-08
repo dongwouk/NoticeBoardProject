22d4b1a410f4df9336fddabae7a8728290934f21
kwon <kwondw30@gmail.com>
2024-10-09 00:50:46 +0900

    user table 수정

--- src/main/java/com/example/noticeboardprj/entity/UserEntity.java
+++ src/main/java/com/example/noticeboardprj/entity/UserEntity.java
@@ -1,11 +1,12 @@
 package com.example.noticeboardprj.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
@@ -15,7 +16,19 @@ public class UserEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    @Column(unique = true, length = 20)
     private String username;
+    @Column(length = 255)
     private String password;
+    @Column(length = 50)
     private String role;
+    @Column(length = 50)
+    private String email;
+    @CreationTimestamp
+    @Column(updatable = false)
+    private LocalDateTime created_date;
+    @UpdateTimestamp
+    @Column(insertable = false)
+    private LocalDateTime modified_date;
+    private LocalDateTime removed_date;
 }
