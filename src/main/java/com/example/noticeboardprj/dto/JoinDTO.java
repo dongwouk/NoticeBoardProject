b1b52e4c06147afbda5db927964f294e0f8ab7fa
kwon <kwondw30@gmail.com>
2024-10-09 00:53:58 +0900

    email, 생성/수정/삭제시간 추가

--- src/main/java/com/example/noticeboardprj/dto/JoinDTO.java
+++ src/main/java/com/example/noticeboardprj/dto/JoinDTO.java
@@ -3,10 +3,18 @@ package com.example.noticeboardprj.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
+
 @Getter
 @Setter
 public class JoinDTO {
     private String username;
     private String password;
+    private String email;
+    private LocalDateTime created_date;
+    private LocalDateTime modified_date;
+    private LocalDateTime removed_date;
+
+
 
 }
