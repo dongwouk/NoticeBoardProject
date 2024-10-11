7b1297c42746ab99aa195d4c2fb722f97270345a
kwon <kwondw30@gmail.com>
2024-10-11 09:57:16 +0900

    refresh token rotate controller

---
+++ src/main/java/com/example/noticeboardprj/controller/ReissueController.java
@@ -0,0 +1,30 @@
+package com.example.noticeboardprj.controller;
+
+import com.example.noticeboardprj.jwt.JWTUtil;
+import com.example.noticeboardprj.service.JoinService;
+import com.example.noticeboardprj.service.ReissueService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody
+public class ReissueController {
+
+    private final ReissueService reissueService;
+
+    public ReissueController(ReissueService reissueService) {
+        this.reissueService = reissueService;
+    }
+
+    @PostMapping("/reissue")
+    public String reissue(HttpServletRequest request, HttpServletResponse response) {
+
+        reissueService.reissue(request, response);
+        return "reissue";
+    }
+
+}
