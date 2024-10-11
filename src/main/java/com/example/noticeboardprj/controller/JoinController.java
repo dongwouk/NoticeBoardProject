a65d4adabcb1f47fbb0137168dc7fafafc4ceba9
kwon <kwondw30@gmail.com>
2024-10-11 09:52:40 +0900

    join controller 추가

---
+++ src/main/java/com/example/noticeboardprj/controller/JoinController.java
@@ -0,0 +1,26 @@
+package com.example.noticeboardprj.controller;
+
+import com.example.noticeboardprj.dto.JoinDTO;
+import com.example.noticeboardprj.service.JoinService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody
+public class JoinController {
+
+    private final JoinService joinService;
+
+    public JoinController(JoinService joinService) {
+        this.joinService = joinService;
+
+    }
+
+    @PostMapping("/join")
+    public String joinProcess(JoinDTO joinDTO) {
+
+        joinService.joinProcess(joinDTO);
+        return "join";
+    }
+}
